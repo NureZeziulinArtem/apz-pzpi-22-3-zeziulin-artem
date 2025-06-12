daf0b262cb5f57fc8765c98d67200ef9f5db0ab2
Зезюлін Артем <artem.zeziulin@nure.ua>
2025-06-12 04:00:10 +0200

    Update SimpleMazeBuilder.cs

--- Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderMaze/SimpleMazeBuilder.cs
+++ Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderMaze/SimpleMazeBuilder.cs
@@ -1,1 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderMaze
+{
+    public class SimpleMazeBuilder : IMazeBuilder
+    {
+        private Maze _maze = new();
+
+        public void AddRoom(string roomName)
+        {
+            if (!_maze.Rooms.Contains(roomName))
+            {
+                _maze.Rooms.Add(roomName);
+            }
+        }
+
+        public void AddPath(string fromRoom, string toRoom)
+        {
+            if (_maze.Rooms.Contains(fromRoom) && _maze.Rooms.Contains(toRoom))
+            {
+                _maze.Paths.Add($"{fromRoom} <--> {toRoom}");
+            }
+            else
+            {
+                Console.WriteLine($"Cannot create path: one or both rooms do not exist.");
+            }
+        }
+
+        public Maze GetMaze()
+        {
+            return _maze;
+        }
+
+        public void Reset()
+        {
+            _maze = new();
+        }
+    }
+}
 
