b568701a9d1c46db44ed53f35d77e1c256d51a7c
Зезюлін Артем <artem.zeziulin@nure.ua>
2025-06-12 03:50:11 +0200

    Update IReportBuilder.cs

--- Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderReport/IReportBuilder.cs
+++ Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderReport/IReportBuilder.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderReport
+{
+    public interface IReportBuilder
+    {
+        void Reset();
+        void BuildHeader();
+        void BuildBody();
+        void BuildFooter();
+        Report GetReport();
+    }
+}
 
