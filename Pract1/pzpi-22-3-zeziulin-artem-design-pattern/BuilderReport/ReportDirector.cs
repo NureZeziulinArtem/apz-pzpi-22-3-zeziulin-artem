0e39fe6ddb79dbce05b3499a591cc7b0715920eb
Зезюлін Артем <artem.zeziulin@nure.ua>
2025-06-12 03:56:05 +0200

    Update ReportDirector.cs

--- Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderReport/ReportDirector.cs
+++ Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderReport/ReportDirector.cs
@@ -1,1 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderReport
+{
+    public class ReportDirector
+    {
+        private IReportBuilder _builder;
+
+        public ReportDirector(IReportBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public void ChangeBuilder(IReportBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public void Make()
+        {
+            _builder.BuildHeader();
+            _builder.BuildBody();
+            _builder.BuildFooter();
+        }
+    }
+}
 
