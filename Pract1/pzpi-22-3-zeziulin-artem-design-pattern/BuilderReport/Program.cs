111b21fa6d60a1dbc6106ca66c69803768b4db57
Зезюлін Артем <artem.zeziulin@nure.ua>
2025-06-12 03:54:40 +0200

    Update Program.cs

--- Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderReport/Program.cs
+++ Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderReport/Program.cs
@@ -1,1 +1,23 @@
+namespace BuilderReport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IReportBuilder plainBuilder = new PlainTextReportBuilder();
+            var director = new ReportDirector(plainBuilder);
+            director.Make();
+            var plainReport = plainBuilder.GetReport();
+            plainReport.Show();
+
+            Console.WriteLine();
+
+            IReportBuilder htmlBuilder = new HtmlReportBuilder();
+            director.ChangeBuilder(htmlBuilder);
+            director.Make();
+            var htmlReport = htmlBuilder.GetReport();
+            htmlReport.Show();
+        }
+    }
+}
 
