18de163b9e0b410de8b25364527f1e8c17c2da85
Зезюлін Артем <artem.zeziulin@nure.ua>
2025-06-12 03:59:14 +0200

    Update Program.cs

--- Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderMaze/Program.cs
+++ Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderMaze/Program.cs
@@ -1,1 +1,20 @@
+namespace BuilderMaze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IMazeBuilder builder = new SimpleMazeBuilder();
+
+            builder.AddRoom("Entrance");
+            builder.AddRoom("Hallway");
+            builder.AddRoom("Treasure Room");
+            builder.AddPath("Entrance", "Hallway");
+            builder.AddPath("Hallway", "Treasure Room");
+
+            Maze maze = builder.GetMaze();
+            maze.Show();
+        }
+    }
+}
 
