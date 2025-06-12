6bb5131102a2e420fb17fa1b085fc7f925422c36
Зезюлін Артем <artem.zeziulin@nure.ua>
2025-06-12 03:57:45 +0200

    Update IMazeBuilder.cs

--- Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderMaze/IMazeBuilder.cs
+++ Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderMaze/IMazeBuilder.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderMaze
+{
+    public interface IMazeBuilder
+    {
+        void AddRoom(string roomName);
+        void AddPath(string fromRoom, string toRoom);
+        void Reset();
+        Maze GetMaze();
+    }
+}
 
