35711599957341aa30a4171b0b8e6fcf3d6ee48c
Зезюлін Артем <artem.zeziulin@nure.ua>
2025-06-12 03:48:32 +0200

    Update HtmlReportBuilder.cs

--- Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderReport/HtmlReportBuilder.cs
+++ Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderReport/HtmlReportBuilder.cs
@@ -1,1 +1,33 @@
+namespace BuilderReport
+{
+    public class HtmlReportBuilder : IReportBuilder
+    {
+        private Report _report = new Report();
+
+        public void BuildHeader()
+        {
+            _report.Header = "<header><h1>Report Header</h1></header>";
+        }
+
+        public void BuildBody()
+        {
+            _report.Body = "<main><p>This is the body of the report.</p></main>";
+        }
+
+        public void BuildFooter()
+        {
+            _report.Footer = "<footer><p>Report Footer</p></footer>";
+        }
+
+        public Report GetReport()
+        {
+            return _report;
+        }
+
+        public void Reset()
+        {
+            _report = new Report();
+        }
+    }
+}
 
