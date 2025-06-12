80711ae2c2f232c98cf4cc547089345b26492bd4
Зезюлін Артем <artem.zeziulin@nure.ua>
2025-06-12 03:53:31 +0200

    Update PlainTextReportBuilder.cs

--- Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderReport/PlainTextReportBuilder.cs
+++ Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderReport/PlainTextReportBuilder.cs
@@ -1,1 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderReport
+{
+    public class PlainTextReportBuilder : IReportBuilder
+    {
+        private Report _report = new Report();
+
+        public void BuildHeader()
+        {
+            _report.Header = "***** Report Header *****";
+        }
+
+        public void BuildBody()
+        {
+            _report.Body = "This is the body of the report.";
+        }
+
+        public void BuildFooter()
+        {
+            _report.Footer = "***** Report Footer *****";
+        }
+
+        public Report GetReport()
+        {
+            return _report;
+        }
+
+        public void Reset()
+        {
+            _report = new Report();
+        }
+    }
+}
 
