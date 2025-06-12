177ecb46a522205ef72df2a8b551263a9f3c7212
Зезюлін Артем <artem.zeziulin@nure.ua>
2025-06-12 03:55:16 +0200

    Update Report.cs

--- Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderReport/Report.cs
+++ Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderReport/Report.cs
@@ -1,1 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderReport
+{
+    public class Report
+    {
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public string Footer { get; set; }
+
+        public void Show()
+        {
+            Console.WriteLine(Header);
+            Console.WriteLine(Body);
+            Console.WriteLine(Footer);
+        }
+    }
+}
 
