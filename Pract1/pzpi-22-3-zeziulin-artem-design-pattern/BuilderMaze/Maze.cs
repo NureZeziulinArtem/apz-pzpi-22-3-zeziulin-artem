8adaa68918cc7e0978e23358b07950b9a22e4aa5
Зезюлін Артем <artem.zeziulin@nure.ua>
2025-06-12 03:58:39 +0200

    Update Maze.cs

--- Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderMaze/Maze.cs
+++ Pract1/pzpi-22-3-zeziulin-artem-design-pattern/BuilderMaze/Maze.cs
@@ -1,1 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderMaze
+{
+    public class Maze
+    {
+        public List<string> Rooms { get; } = new();
+        public List<string> Paths { get; } = new();
+
+        public void Show()
+        {
+            Console.WriteLine("Rooms:");
+            foreach (var room in Rooms)
+                Console.WriteLine($"- {room}");
+
+            Console.WriteLine("Paths:");
+            foreach (var path in Paths)
+                Console.WriteLine($"- {path}");
+        }
+    }
+}
 
